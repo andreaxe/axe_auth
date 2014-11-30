65be1d048b5b985b8324881a571c7fab5a4eb564
mrekko <andregarxia@gmail.com>
2014-11-30 19:51:56 +0000

    library update

--- application/libraries/Axe_auth.php
+++ application/libraries/Axe_auth.php
@@ -13,8 +13,49 @@ class Axe_auth
         $this->load->config('axe_auth');
         $this->load->library('email');
         $this->load->model('axe_auth_model');
+
+        // Load the session, CI2 as a library, CI3 uses it as a driver
+        if (substr(CI_VERSION, 0, 1) == '2')
+        {
+            $this->load->library('session');
+        }
+        else
+        {
+            $this->load->driver('session');
+        }
+
+    }
+
+    /**
+     * @param $username
+     * @param $password
+     * @param $ip
+     */
+    public function login ($username,$password, $ip)
+    {
+        $result = $this->axe_auth_model->check_credentials($username,$password);
+        $result ? $this->whitelist($ip) :
+    }
+
+    /**
+     * @param $ip
+     */
+    public function whitelist ($ip)
+    {
+
+    }
+
+    /**
+     * @param $ip
+     */
+    public function blacklist ($ip)
+    {
+
     }
 
 
-
-} 
+    public function checkAttempts ($username, $ip)
+    {
+        
+    }
+}
